2b6996488b340bc04993bcaaf2e1e9cb563afdea
HoaiHao <hoaihaopro@gmail.com>
2021-03-25 16:30:47 +0700

    RECURSION UPDATE & GREEN  AGAIN

--- src/com/haonlh/mathutil/MathUtility.java
+++ src/com/haonlh/mathutil/MathUtility.java
@@ -23,17 +23,29 @@ public class MathUtility {
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("n must be between 0..20");
         }
-
-        // sống xót đên đây thì n = 0 ,1 .. 20
-        if (n == 0 || n == 1) {
-            return 1; // 0!, 1!
+        
+        // sửa code, sửa hàm giai thừa cho gọn, dùng đệ quy
+        
+        if(n == 0 || n == 1){
+            return 1; // 0! = 1! = 1 tính luôn cho nhanh
         }
+        return n * getFactorial(n - 1);  // gọi lại chính mình với quy mô khác(nhỏ hơn)
+        // n! = n * (n -1)!
+        // đệ quy, vòng lại chính mình với quy mô khác
+        // recursion. Làm khéo nếu ko có đi mãi mãi
+        // STACKOVERFLOW.COM
+        
 
-        long result = 1; // giả bộ
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-        }
-
-        return result; // nhân từ 2 3 4 5 6 7 8
+//        // sống xót đên đây thì n = 0 ,1 .. 20
+//        if (n == 0 || n == 1) {
+//            return 1; // 0!, 1!
+//        }
+//
+//        long result = 1; // giả bộ
+//        for (int i = 1; i <= n; i++) {
+//            result *= i;
+//        }
+//
+//        return result; // nhân từ 2 3 4 5 6 7 8
     }
 }
